6857d7ffa26156d9a62ae783ef362382e57a0d5e
BoomKhqii <boom.raeiy@gmail.com>
2026-02-07 23:23:23 +0800

    Added visible table | error whenever adding an dr/iv

--- Kool Ref Inventory System/Pages/Inventory.cshtml.cs
+++ Kool Ref Inventory System/Pages/Inventory.cshtml.cs
@@ -17,11 +17,11 @@ namespace Kool_Ref_Inventory_System.Pages
         [BindProperty] public string Location { get; set; }
         [BindProperty] public int DeliveryReceipt { get; set; }
         [BindProperty] public int InVoice { get; set; }
+        string connectionString = "Server=localhost\\SQLEXPRESS;Database=master;Trusted_Connection=True;TrustServerCertificate=True;";
+        public List<Items> Inventory { get; set; }
 
         public void OnPost()
         {
-            string connectionString = "Server=localhost\\SQLEXPRESS;Database=master;Trusted_Connection=True;TrustServerCertificate=True;";
-
             using (SqlConnection conn = new SqlConnection(connectionString))
             {
                 conn.Open();
@@ -51,87 +51,47 @@ namespace Kool_Ref_Inventory_System.Pages
             }
         }
 
-
-        /*
-        public void OnPost()
+        public void OnGet()
         {
-
-            string connectionString = "Server=.\\SQLEXPRESS;Database=master;Trusted_Connection=True;TrustServerCertificate=True;";
-
-
             using (SqlConnection conn = new SqlConnection(connectionString))
             {
                 conn.Open();
-                string query = "INSERT INTO dbo.InandOutSystem (Item, Description, Supplier, Quantity, Price, InOut, Location) VALUES (@item, @description, @supplier, @quantity, @price, @inOut, @location)";
+                string query = "SELECT * FROM dbo.InandOutSystem";
+
                 using (SqlCommand cmd = new SqlCommand(query, conn))
                 {
-                    cmd.Parameters.AddWithValue("@item", Item);
-                    cmd.Parameters.AddWithValue("@description", Description);
-                    cmd.Parameters.AddWithValue("@supplier", Supplier);
-                    cmd.Parameters.AddWithValue("@quantity", Quantity);
-                    cmd.Parameters.AddWithValue("@price", Price);
-                    cmd.Parameters.AddWithValue("@inOut", InOut);
-                    cmd.Parameters.AddWithValue("@location", Location);
-                    cmd.ExecuteNonQuery();
-                }
-
-                /*
-                public void OnGet()
-                {
-                    using var conn = new MySqlConnection(
-                        "server=localhost;user=root;password=1234;database=testdb");
-                    // Server=localhost\SQLEXPRESS01;Database=master;Trusted_Connection=True;
-
-                    conn.Open();
-                }
-                */
-        // Source - https://stackoverflow.com/a/1345531
-        // Posted by marc_s, modified by community. See post 'Timeline' for change history
-        // Retrieved 2026-01-04, License - CC BY-SA 4.0
-
-        //string connectionString = "server=localhost;database=Inventory;integrated Security=SSPI;";
-        //string connectionString = builder.Configuration.GetConnectionString("Inventory");
-        /*
-        using(SqlConnection _con = new SqlConnection(connectionString))
-        {
-           string queryStatement = "SELECT TOP 5 * FROM dbo.Customers ORDER BY CustomerID";
-
-           using(SqlCommand _cmd = new SqlCommand(queryStatement, _con))
-           {
-              DataTable customerTable = new DataTable("Top5Customers");
-
-    SqlDataAdapter _dap = new SqlDataAdapter(_cmd);
-
-    _con.Open();
-              _dap.Fill(customerTable);
-              _con.Close();
-
-           }
-        }
-    */
-        /*
-        [HttpPost]
-        public IActionResult UpdateStatus(int id, bool status)
-        {
-            string connString = "DESKTOP-8KJ2D7L";
-
-            using (SqlConnection conn = new SqlConnection(connString))
-            {
-                // This is the SQL command sent to SSMS
-                string sql = "UPDATE Users SET IsIn = @status WHERE UserID = @id";
-                SqlCommand cmd = new SqlCommand(sql, conn);
-
-                cmd.Parameters.AddWithValue("@id", id);
-                cmd.Parameters.AddWithValue("@status", status);
-
-                conn.Open();
-                cmd.ExecuteNonQuery();
-            }
-            return Ok("Status Updated in SSMS!");
-        }*/
-        /*
+                    using (SqlDataReader reader = cmd.ExecuteReader())
+                    {
+                        Inventory = new List<Items>();
+                        while (reader.Read())
+                        {
+                            Inventory.Add(new Items
+                            {
+                                Item = reader["Item"].ToString(),
+                                Description = reader["Description"].ToString(),
+                                Supplier = reader["Supplier"].ToString(),
+                                Quantity = Convert.ToInt32(reader["Quantity"]),
+                                Price = Convert.ToDecimal(reader["Price"]),
+                                Location = reader["Location"].ToString(),
+                                DeliveryReceipt = reader["DeliveryReceipt"] == DBNull.Value ? 0 : Convert.ToInt32(reader["DeliveryReceipt"]),
+                                InVoice = reader["InVoice"] == DBNull.Value ? 0 : Convert.ToInt32(reader["InVoice"])
+                            });
+                        }
                     }
                 }
-            */
+            }
+        }
+    }
+
+    public class Items
+    {
+        public string Item { get; set; }
+        public string Description { get; set; }
+        public string Supplier { get; set; }
+        public int Quantity { get; set; }
+        public decimal Price { get; set; }
+        public string Location { get; set; }
+        public int DeliveryReceipt { get; set; }
+        public int InVoice { get; set; }
     }
 }
