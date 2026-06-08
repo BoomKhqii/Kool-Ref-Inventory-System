0575048a07e31ca6cd96dc8f4bbdb09115e49916
BoomKhqii <aeijaeiraeiy.delossantos@gmail.com>
2026-06-09 01:02:32 +0800

    Designed Login Page; Added Hidden Create account; Added Login System; Implemented BCrypt in the password; Changed launched settings to starting with Login Page; Disabled Layout in Login page

---
+++ Kool Ref Inventory System/Pages/Login.cshtml.cs
@@ -0,0 +1,77 @@
+using BCrypt.Net;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Data.SqlClient;
+using System.Net;
+using System.Timers;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+
+namespace Kool_Ref_Inventory_System.Pages
+{
+    public class LoginModel : PageModel
+    {
+        [BindProperty] public string Username { get; set; }
+        [BindProperty] public string Password { get; set; }
+
+        string connectionString = "Server=localhost\\SQLEXPRESS;Database=Koolref;Trusted_Connection=True;TrustServerCertificate=True;";
+
+        /*
+         * Login Test
+  
+        public IActionResult OnPost()
+        {
+            using (SqlConnection conn = new SqlConnection(connectionString))
+            {
+                conn.Open();
+                string detailsQuery = "INSERT INTO Koolref.dbo.UserDetails (Username, Password) VALUES (@username, @password)";
+
+                using (SqlCommand cmd = new SqlCommand(detailsQuery, conn))
+                {
+
+                    cmd.Parameters.AddWithValue("@username", Username);
+                    cmd.Parameters.AddWithValue("@password", HashBCrypt(Password));
+                    cmd.ExecuteNonQuery();
+                }
+            }
+            return RedirectToPage("/Login");
+        }
+        */
+
+        public IActionResult OnPost()
+        {
+            using (SqlConnection conn = new SqlConnection(connectionString))
+            {
+                conn.Open();
+
+                string query = "SELECT Password FROM Koolref.dbo.UserDetails WHERE Username = @username";
+
+                using (SqlCommand cmd = new SqlCommand(query, conn))
+                {
+                    cmd.Parameters.AddWithValue("@username", Username);
+
+                    object result = cmd.ExecuteScalar();
+
+                    if (result == null)
+                    {
+                        ModelState.AddModelError("", "Invalid username or password");
+                        return Page();
+                    }
+
+                    string storedHash = result.ToString();
+
+                    bool passwordCorrect = BCrypt.Net.BCrypt.Verify(Password, storedHash);
+
+                    if (!passwordCorrect)
+                    {
+                        ModelState.AddModelError("", "Invalid username or password");
+                        return Page();
+                    }
+                }
+            }
+
+            return RedirectToPage("/Service");
+        }
+
+        public string HashBCrypt(string data) { return BCrypt.Net.BCrypt.HashPassword(data); }
+    }
+}
